e50e6a6504282c663c279ae1006d6f036aac4866
cse18107 <cse18107@cemk.ac.in>
2022-06-06 07:07:43 +0530

    working on getting all users

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -85,6 +85,12 @@ const getUser = async (req, res) => {
 
 const getUsers = async (req, res) => {
   try {
+    const users = await User.find();
+
+    res.status(200).json({
+      success:true,
+      users
+    });
   } catch (error) {
     res.status(400).json({
       success: false,
